057810fde8a3b619bd486ea0b3f56189869a7354
evilrise7 <45169049+evilrise7@users.noreply.github.com>
2019-03-03 18:33:12 +0600

    1-й день

--- yandexwebproject.py
+++ yandexwebproject.py
@@ -1,24 +1,121 @@
-from flask import Flask, render_template
+from flask import Flask, \
+    render_template, redirect, flash, \
+    request, url_for, session
+
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
+from wtforms import Form, StringField, \
+    TextAreaField, PasswordField, validators
 
 
+# Само приложение
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_POSHEL_V_ZHOPU'
+app.config['MYSQL_HOST'] = 'sql9.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql9281617'
+app.config['MYSQL_PASSWORD'] = 'YG8tYECG5Z'
+app.config['MYSQL_DB'] = 'sql9281617'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# База Данных
+mysql = MySQL(app)
 
 
+# Форма Регистрации
+class Registration(Form):
+    # validators.Length = Длина соответсвующего текстового поля
+    name = StringField('Имя', [validators.Length(min=1, max=50)])
+    username = StringField('Портнейм', [validators.Length(min=4, max=25)])
+    email = StringField('ПортМэйл', [validators.Length(min=6, max=50)])
+    password = PasswordField('Пароль', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Пароли не совпадают!')
+    ])
+    confirm = PasswordField('Подтвердить пароль')
+
+
+# Начальная страница
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
 
-@app.route('/login', methods=["GET", "POST"])
+# Регистрация Пользователя
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = Registration(request.form)
+    if request.method == 'POST' and form.validate():
+        # Имя
+        name = form.name.data
+        # Почта
+        email = form.email.data
+        # Имя пользователя
+        username = form.username.data
+        # Перешифрование пароля
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Инициализация курсора
+        cursor = mysql.connection.cursor()
+        # Занесение данных в Базу Данных
+        cursor.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        # Занесение изменений в Базу Данных
+        mysql.connection.commit()
+
+        # Выход курсора
+        cursor.close()
+        flash('Вы успешно зарегистрированы и теперь можете приступить к работе',
+              'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+# Вход пользователя
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Получаю данные из текстовых форм
+        # Пользователь
+        port_name = request.form['username']
+        # Пароль
+        port_pass = request.form['password']
+
+        # Инициализация курсора
+        cursor = mysql.connection.cursor()
+        # Проверка пользователя в наличие в БД
+        result = cursor.execute("SELECT * FROM users WHERE username = %s", [port_name])
+
+        if result > 0:
+            data = cursor.fetchone()
+            password_crypt = data['password']
+
+            # Сравнение паролей(зашифрованного и обычного)
+            if sha256_crypt.verify(port_pass, password_crypt):
+                # Вход был произведен
+                session['logged_in'] = True
+                session['username'] = port_name
+
+                flash('Вы уже зашли в аккаунт.', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                # Были введены некорректные данные
+                error = 'Неверный логин'
+                return render_template('login.html', error=error)
+            # Выход курсора
+            cursor.close()
+        else:
+            # Если в базе данных не было найдено пользователя
+            error = 'Портнейм не найден!'
+            return render_template('login.html', error=error)
+
     return render_template('login.html')
 
 
+# О проекте
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
+# Запуск программы
 if __name__ == '__main__':
-    app.run(port=2555, host='127.0.0.1')
+    app.run(port=5000, host='localhost')
